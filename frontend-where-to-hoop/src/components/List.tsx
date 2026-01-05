3cb2c7ea13bbb561e45990aba56e3a033ea2f6a6
renssim1 <mark.renssi@aalto.fi>
2026-01-05 16:43:36 +0200

    feat: implement search field filtering for the hoop cards

--- frontend-where-to-hoop/src/components/List.tsx
+++ frontend-where-to-hoop/src/components/List.tsx
@@ -6,7 +6,7 @@ import { useMediaQuery } from 'usehooks-ts'
 import { Link } from "react-router-dom";
 import haversineDistance from "../utils/functions";
 import { useLocationValues } from "../contexts/LocationContext";
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import useLocateUser from "../hooks/useLocateUser";
 import { SearchField } from "./reusable/SearchField";
 
@@ -16,6 +16,7 @@ interface ListProps {
 }
 
 const List = ({ toggleFunction, mapView }: ListProps) => {
+  const [searchTerm, setSearchTerm] = useState("");
   const xmd = useMediaQuery(`(min-width: ${breakpoints.xmd})`);
   const mapCenterValues = useLocationValues();
   const locateUser = useLocateUser();
@@ -42,14 +43,25 @@ const List = ({ toggleFunction, mapView }: ListProps) => {
     .sort((a, b) => a.distance - b.distance);
 }, [mapCenterValues.latitude, mapCenterValues.longitude]);
 
+
+  const filteredHoops = useMemo(() => {
+    return sortedHoopsWithDistance.filter(({ hoop }) =>
+      hoop.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }, [searchTerm, sortedHoopsWithDistance]);
+
   return (
     <div className="pt-40 h-[100vh] w-[100vw] overflow-y-auto padding-b-for-page">
       <div className="absolute w-1/2 top-19 right-[10px]">
-        <SearchField placeholder="Find hoops"/>
+        <SearchField 
+          placeholder="Find hoops" 
+          value={searchTerm}
+          onChange={setSearchTerm}
+        />
       </div>
     {xmd ? (
       <div className="grid grid-cols-2 2xl:grid-cols-3 grid-rows-2 gap-6 px-8">
-        {sortedHoopsWithDistance.map(({ hoop, distance }) => (
+        {filteredHoops.map(({ hoop, distance }) => (
         <Link key={hoop.id} to={`#`}> {/* Temporary link to prevent navigation on small screens -> /hoops/${hoop.id}*/}
           <HoopCard hoop={hoop} distance={distance} toggleFunction={toggleFunction} mapView={mapView} />
         </Link>
--- frontend-where-to-hoop/src/components/reusable/SearchField.tsx
+++ frontend-where-to-hoop/src/components/reusable/SearchField.tsx
@@ -1,6 +1,6 @@
 import {
   SearchField as AriaSearchField,
-  Input, FieldError, Label, Group, Button,
+  Input, FieldError, Label, Group,
   type SearchFieldProps as AriaSearchFieldProps,
   type ValidationResult,
 } from 'react-aria-components';
@@ -14,9 +14,11 @@ interface SearchFieldProps extends AriaSearchFieldProps {
   label?: string;
   errorMessage?: string | ((validation: ValidationResult) => string);
   placeholder?: string;
+  value: string;
+  onChange: (value: string) => void;
 }
 
-const SearchField = ({ label, errorMessage, placeholder }: SearchFieldProps) => {
+const SearchField = ({ label, errorMessage, placeholder, value, onChange }: SearchFieldProps) => {
   const colorModeContext: ColorMode = useColorModeValues();
   
   return (
@@ -27,10 +29,12 @@ const SearchField = ({ label, errorMessage, placeholder }: SearchFieldProps) => 
         </Label>
       )}
       <Group className={`${colorModeContext} w-full flex items-center gap-2 pl-3 py-1.5 border-maplabel rounded-lg bg-background focus-within:ring-2 focus-within:ring-gray-500 transition-shadow dark:border-gray-600`}>
-        <FaSearch aria-hidden className={`${colorModeContext} flex-none w-4 h-4 text-gray-500 hover:text-black transition-colors dark:text-white cursor-pointer dark:hover:text-gray-300`} />
+        <FaSearch aria-hidden className={`${colorModeContext} flex-none w-4 h-4 text-black hover:text-black transition-colors dark:text-white cursor-pointer dark:hover:text-gray-300`} />
         <Input 
           placeholder={placeholder} 
           className={`${colorModeContext} min-w-2 flex-1 px-2 bg-transparent outline-none text-gray-900 placeholder:text-gray-400 dark:text-white  dark:placeholder:text-gray-500`} 
+          value={value}
+          onChange={(e) => onChange(e.currentTarget.value)}
         />
         {/*Clear button - functionality to be added later 
         <Button className={`${colorModeContext} flex-none w-5 h-5 text-gray-500 hover:text-gray-700 transition-colors dark:text-white dark:hover:text-gray-300`}>
